e05762696a2ddb27f2637e705aed92070960943b
Leya Joy <82429107+leyajoy@users.noreply.github.com>
2024-04-05 22:46:14 +0530

    authentication added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './global.css'
-import {BrowserRouter as Router} from "react-router-dom"
-import AppRoutes from './AppRoutes'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Router>
-      <AppRoutes/>
-    </Router>
-  </React.StrictMode>,
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<Router>
+			<Auth0ProviderWithNavigate>
+				<AppRoutes />
+			</Auth0ProviderWithNavigate>
+		</Router>
+	</React.StrictMode>
 );
